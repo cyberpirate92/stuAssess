de1cdd5ab386678bc5265dc39c763a3097a530aa
Ravi Teja <bsravi.tez@gmail.com>
2015-09-27 13:55:47 +0530

    Added comment

--- student_login_check.php
+++ student_login_check.php
@@ -11,6 +11,7 @@
 	{
 		require_once('util.php');
 		session_destroy();
+		// display alert
 		displayJSAlert('You must be logged in to view this page, please login');
 		redirectTo('login.php');
 		die('');
